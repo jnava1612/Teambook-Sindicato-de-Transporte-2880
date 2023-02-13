d46d14edb598dead7ff1d540e9c84ea9199a8530
jnava1612 <navajorgeeduardo@gmail.com>
2023-02-13 00:12:06 -0400

    Modified Sieve Algorithms

--- Mathematics/Euclid.cpp
+++ Mathematics/Euclid.cpp
@@ -10,21 +10,3 @@ void extendedEuclid(int a, int b)//ecuacion diofantica ax + by = d
 	y = x-(a/b)*y;
 	x=x1;
 }
-
-
-Encuentra x, y en la ecuación de la forma ax + by = c             
-Agregar Extended Euclides.
-
-ll g;
-bool diophantine(ll a, ll b, ll c) {
-	x = y = 0;
-	if(!a && !b) return (!c); // sólo hay solución con c = 0
-	g = euclid(abs(a), abs(b));
-	if(c % g) return false;
-	a /= g; b /= g; c /= g;
-	if(a < 0) x *= -1;
-	x = (x % b) * (c % b) % b;
-	if(x < 0) x += b;
-	y = (c - a*x) / b;
-	return true;
-}

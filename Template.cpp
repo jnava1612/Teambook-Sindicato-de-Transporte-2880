671519a9860a62acd23268a4451f64cbf983e2d7
jnava1612 <navajorgeeduardo@gmail.com>
2023-03-02 12:06:28 -0400

    Added the Template

---
+++ Template.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// #include <ext/rope>
+#define int ll
+#define mp        make_pair
+#define pb        push_back
+#define all(a)      (a).begin(), (a).end()
+#define sz(a)      (int)a.size()
+#define eq(a, b)       (fabs(a - b) < EPS)
+#define md(a, b)    ((a) % b + b) % b
+#define mod(a)      md(a, MOD)
+#define _max(a, b) ((a) > (b) ? (a) : (b))
+#define srt(a)      sort(all(a))
+#define mem(a, h)    memset(a, (h), sizeof(a))
+#define f         first
+#define s         second
+#define forn(i, n)      for(int i = 0; i < n; i++)
+#define fore(i, b, e)  for(int i = b; i < e; i++)
+#define forg(i, b, e, m)  for(int i = b; i < e; i+=m)
+#define index  int mid = (b + e) / 2, l = node * 2 + 1, r = l + 1;
+#define DBG(x) cerr<<#x<<" = "<<(x)<<endl
+#define RAYA cout<<"=============================="<<'\n'
+// int in(){int r=0,c;for(c=getchar();c<=32;c=getchar());if(c=='-') return -in();for(;c>32;r=(r<<1)+(r<<3)+c-'0',c=getchar());return r;}
+
+
+using namespace std;
+// using namespace __gnu_pbds;
+// using namespace __gnu_cxx;
+
+// #pragma GCC target ("avx2")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+
+typedef long long     ll;
+typedef long double ld;
+typedef unsigned long long     ull;
+typedef pair<int, int>  ii;
+typedef pair<pair<int, int>, int> iii;
+typedef vector<int>     vi;
+typedef vector<ii>      vii;
+typedef vector<ll>      vll;
+// typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+// find_by_order kth largest  order_of_key <
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// rng
+const int tam = 200010;
+const int MOD = 1000000007;
+const int MOD1 = 998244353;
+const double DINF=1e100;
+const double EPS = 1e-9;
+const long double PI  = acos(-1.0L);
+
+
+
+signed main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    // freopen("asd.txt", "r", stdin);
+    // freopen("qwe.txt", "w", stdout);
+    
+
+    return 0;
+}
+

b59e9274d82aab6a4b07ede39db83543ea5764ed
jnava1612 <navajorgeeduardo@gmail.com>
2023-03-02 12:43:42 -0400

    Added Dinic section on Flows

---
+++ Graphs/Dinic.cpp
@@ -0,0 +1,66 @@
+struct flowEdge
+{
+    int to, rev, f, cap;
+};
+
+vector<vector<flowEdge> > G;
+
+
+void addEdge(int st, int en, int cap) { 
+    // Anade arista (st --> en) con su capacidad
+    flowEdge A = {en, (int)G[en].size(), 0, cap}; 
+    flowEdge B = {st, (int)G[st].size(), 0, 0};
+    G[st].pb(A);
+    G[en].pb(B);
+}
+
+int nodes, S, T; // asignar estos valores al armar el grafo G
+                 // nodes = nodos en red de flujo. Hacer G.clear(); G.resize(nodes);
+vi work, lvl;
+int Q[200010];
+
+bool bfs() {
+    int qt = 0;
+    Q[qt++] = S;
+    lvl.assign(nodes, -1);
+    lvl[S] = 0;
+    for (int qh = 0; qh < qt; qh++) {
+        int v = Q[qh];
+        for (flowEdge &e : G[v]) {
+            int u = e.to;
+            if (e.cap <= e.f || lvl[u] != -1) continue;
+            lvl[u] = lvl[v] + 1;
+            Q[qt++] = u;
+        }
+    }
+    return lvl[T] != -1;
+}
+
+int dfs(int v, int f) {
+    if (v == T || f == 0) return f;
+    for (int &i = work[v]; i < G[v].size(); i++) {
+        flowEdge &e = G[v][i];
+        int u = e.to;
+        if (e.cap <= e.f || lvl[u] != lvl[v] + 1) continue;
+        int df = dfs(u, min(f, e.cap - e.f));
+        if (df) {
+            e.f += df;
+            G[u][e.rev].f -= df;
+            return df;
+        }
+    }
+    return 0;
+}
+
+int maxFlow() {
+    int flow = 0;
+    while (bfs()) {
+        work.assign(nodes, 0);
+        while (true) {
+            int df = dfs(S, INF);
+            if (df == 0) break;
+            flow += df;
+        }
+    }
+    return flow;
+}

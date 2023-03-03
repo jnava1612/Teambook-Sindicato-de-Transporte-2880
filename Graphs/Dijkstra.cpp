38c05bf6b86605b234c3472b6655626bcd1e13c0
jnava1612 <navajorgeeduardo@gmail.com>
2023-03-03 18:11:52 -0400

    Fixed Path Solving for Dijkstra

--- Graphs/Dijkstra.cpp
+++ Graphs/Dijkstra.cpp
@@ -36,8 +36,12 @@ void dijkstra(int s, vector<int> & d, vector<int> & p) {
 vector<int> restore_path(int s, int t, vector<int> const& p) {
     vector<int> path;
 
-    for (int v = t; v != s; v = p[v])
+    for (int v = t; v != s; v = p[v]){
+        if(v == -1){
+            return {};
+        }
         path.push_back(v);
+    }
     path.push_back(s);
 
     reverse(path.begin(), path.end());

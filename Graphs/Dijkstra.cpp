8218c4e6bb087e96f112af6c4cf7bb1a36ef709d
jnava1612 <navajorgeeduardo@gmail.com>
2023-03-03 17:37:52 -0400

    Added Dijkstra

---
+++ Graphs/Dijkstra.cpp
@@ -0,0 +1,45 @@
+const int INF = 1e9;
+vector<vector<pair<int, int>>> adj;  //To store the node to which the edge flows to and the weight of the edge
+
+void dijkstra(int s, vector<int> & d, vector<int> & p) {
+    int n = adj.size();
+    d.assign(n, INF);
+    p.assign(n, -1);
+    vector<bool> u(n, false);
+
+    d[s] = 0;
+    for (int i = 0; i < n; i++) {
+        int v = -1;
+        for (int j = 0; j < n; j++) {
+            if (!u[j] && (v == -1 || d[j] < d[v]))
+                v = j;
+        }
+
+        if (d[v] == INF)
+            break;
+
+        u[v] = true;
+        for (auto edge : adj[v]) {
+            int to = edge.first;
+            int len = edge.second;
+
+            if (d[v] + len < d[to]) {
+                d[to] = d[v] + len;
+                p[to] = v;
+            }
+        }
+    }
+}
+
+//In order to restore the path, we need to store the parent of each node in the shortest path tree
+
+vector<int> restore_path(int s, int t, vector<int> const& p) {
+    vector<int> path;
+
+    for (int v = t; v != s; v = p[v])
+        path.push_back(v);
+    path.push_back(s);
+
+    reverse(path.begin(), path.end());
+    return path;
+}

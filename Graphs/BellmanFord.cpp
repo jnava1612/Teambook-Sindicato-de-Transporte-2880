5ea37d2b22271687ce6b4d25d612f2c118ca0e7b
jnava1612 <navajorgeeduardo@gmail.com>
2023-03-04 11:54:30 -0400

    Added Bellman Ford

---
+++ Graphs/BellmanFord.cpp
@@ -0,0 +1,165 @@
+struct edge
+{
+    int a, b, cost;
+};
+
+int n, m, v;
+vector<edge> e;
+const int INF = 1e9;
+
+void solve()
+{
+    vector<int> d (n, INF);
+    d[v] = 0;
+    for (int i=0; i<n-1; ++i)
+        for (int j=0; j<m; ++j)
+            if (d[e[j].a] < INF) // is needed only if the graph contains negative weight edges: no such verification would result in relaxation from the vertices to which paths have not yet found, and incorrect distance
+                d[e[j].b] = min (d[e[j].b], d[e[j].a] + e[j].cost);
+    // display d, for example, on the screen
+}
+
+
+// An improvement in the time of the algorithm could be introduced by keeping the flag so that no time is wasted in visiting all edges.
+
+void solve()
+{
+    vector<int> d (n, INF);
+    d[v] = 0;
+    for (;;)
+    {
+        bool any = false;
+
+        for (int j=0; j<m; ++j)
+            if (d[e[j].a] < INF)
+                if (d[e[j].b] > d[e[j].a] + e[j].cost)
+                {
+                    d[e[j].b] = d[e[j].a] + e[j].cost;
+                    any = true;
+                }
+
+        if (!any) break;
+    }
+    // display d, for example, on the screen
+}
+
+//To retrieve the path of thr Bellman Ford algorithm, you need to keep the previous vertex in the path, and then go back from the end to the beginning.
+
+void solve()
+{
+    vector<int> d (n, INF);
+    d[v] = 0;
+    vector<int> p (n, -1);
+
+    for (;;)
+    {
+        bool any = false;
+        for (int j = 0; j < m; ++j)
+            if (d[e[j].a] < INF)
+                if (d[e[j].b] > d[e[j].a] + e[j].cost)
+                {
+                    d[e[j].b] = d[e[j].a] + e[j].cost;
+                    p[e[j].b] = e[j].a;
+                    any = true;
+                }
+        if (!any)  break;
+    }
+
+    if (d[t] == INF)
+        cout << "No path from " << v << " to " << t << ".";
+    else
+    {
+        vector<int> path;
+        for (int cur = t; cur != -1; cur = p[cur])
+            path.push_back (cur);
+        reverse (path.begin(), path.end());
+
+        cout << "Path from " << v << " to " << t << ": ";
+        for (size_t i=0; i<path.size(); ++i)
+            cout << path[i] << ' ';
+    }
+}
+
+
+//Negative cycle detection 
+
+void solve()
+{
+    vector<int> d (n, INF);
+    d[v] = 0;
+    vector<int> p (n, - 1);
+    int x;
+    for (int i=0; i<n; ++i)
+    {
+        x = -1;
+        for (int j=0; j<m; ++j)
+            if (d[e[j].a] < INF)
+                if (d[e[j].b] > d[e[j].a] + e[j].cost)
+                {
+                    d[e[j].b] = max (-INF, d[e[j].a] + e[j].cost);
+                    p[e[j].b] = e[j].a;
+                    x = e[j].b;
+                }
+    }
+
+    if (x == -1)
+        cout << "No negative cycle from " << v;
+    else
+    {
+        int y = x;
+        for (int i=0; i<n; ++i)
+            y = p[y];
+
+        vector<int> path;
+        for (int cur=y; ; cur=p[cur])
+        {
+            path.push_back (cur);
+            if (cur == y && path.size() > 1)
+                break;
+        }
+        reverse (path.begin(), path.end());
+
+        cout << "Negative cycle: ";
+        for (size_t i=0; i<path.size(); ++i)
+            cout << path[i] << ' ';
+    }
+}
+
+
+// The SPFA (Shortest Path Faster Algorithm) is an improvement of Bellman Ford which takes advantage of the fact that not all attempts at relaxation will work
+
+const int INF = 1e9;
+vector<vector<pair<int, int>>> adj;
+
+bool spfa(int s, vector<int>& d) {
+    int n = adj.size();
+    d.assign(n, INF);
+    vector<int> cnt(n, 0);
+    vector<bool> inqueue(n, false);
+    queue<int> q;
+
+    d[s] = 0;
+    q.push(s);
+    inqueue[s] = true;
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
+        inqueue[v] = false;
+
+        for (auto edge : adj[v]) {
+            int to = edge.first;
+            int len = edge.second;
+
+            if (d[v] + len < d[to]) {
+                d[to] = d[v] + len;
+                if (!inqueue[to]) {
+                    q.push(to);
+                    inqueue[to] = true;
+                    cnt[to]++;
+                    if (cnt[to] > n)
+                        return false;  // negative cycle
+                }
+            }
+        }
+    }
+    return true;
+}
